c807216db735791942eca4e0384a604f4eb2755e
Anton Yevchik <av.yevchik@gmail.com>
2020-12-08 13:10:04 +0200

    Episode 64

--- resources/views/profiles/show.blade.php
+++ resources/views/profiles/show.blade.php
@@ -8,6 +8,16 @@
                      <h1>
                          {{$profileUser->name}}
                      </h1>
+
+                     @can('update', $profileUser)
+                         <form method="POST" action="{{ route('avatar', $profileUser) }}" enctype="multipart/form-data">
+                            {{csrf_field()}}
+                            <input type="file" name="avatar">
+
+                            <button type="submit" class="btn btn-primary">Add Avatar</button>
+                         </form>
+                     @endcan
+                     <img src="{{asset($profileUser->avatar_path)}}" width="5%" height="5%">
                  </div>
                  @forelse($activities as $date => $activity)
                      <h3 class="page header">{{$date}}</h3>
--- resources/views/threads/show.blade.php
+++ resources/views/threads/show.blade.php
@@ -12,9 +12,10 @@
                     <div class="card mb-4" >
                         <div class="card-header">
                             <div class="level">
+                                <img src="{{asset($thread->creator->avatar())}}" alt="{{ $thread->creator->name }}" width="3%" height="3%" class="mr-1">
                                 <span class="flex">
                                     <a href="{{route('profile', $thread->creator)}}"> {{$thread->creator->name}}</a> posted:
-                            {{$thread->title}}
+                                    {{$thread->title}}
                                 </span>
                                 @can('update',$thread)
                                     <form action="{{$thread->path()}}" method="POST">
