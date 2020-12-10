789782f8b227e570a4de649cef7362285d5fbc72
Anton Yevchik <av.yevchik@gmail.com>
2020-12-10 18:02:33 +0200

    Episode 66

--- resources/views/threads/index.blade.php
+++ resources/views/threads/index.blade.php
@@ -7,6 +7,16 @@
                 @include('threads._list')
                 {{ $threads->render() }}
             </div>
+            <div class="col-md-4">
+            <div class="card">
+                <div class="card-header">
+                    Trending Threads
+                </div>
+                <div class="card-body">
+                     sdf
+                </div>
+            </div>
+            </div>
         </div>
     </div>
 @endsection
