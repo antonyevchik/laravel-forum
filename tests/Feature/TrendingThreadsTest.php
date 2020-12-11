8fc7bda2b671e60de58d9054116360a4ecec7751
Anton Yevchik <av.yevchik@gmail.com>
2020-12-11 14:05:23 +0200

    Episode 66

--- tests/Feature/TrendingThreadsTest.php
+++ tests/Feature/TrendingThreadsTest.php
@@ -6,19 +6,29 @@ use Illuminate\Foundation\Testing\DatabaseMigrations;
 
 use Illuminate\Support\Facades\Redis;
 use Tests\TestCase;
+use function GuzzleHttp\json_encode;
 
 class TrendingThreadsTest extends TestCase
 {
     use DatabaseMigrations;
+
+    protected function setUp():void
+    {
+        parent::setUp();
+        Redis::del('trending_threads');
+    }
     /** @test */
     public function it_increments_a_threads_score_each_time_it_is_read()
     {
+        $this->assertEmpty(Redis::zrevrange('trending_threads', 0, -1));
+
         $thread = create('App\Thread');
 
         $this->call('GET', $thread->path());
+        $trending = Redis::zrevrange('trending_threads', 0, -1);
 
-        $d = Redis::zrevrange('trending_threads', 0, -1);
+        $this->assertCount(1, $trending);
 
-        dd($d);
+        $this->assertEquals($thread->title, json_decode($trending[0])->title);
     }
 }
