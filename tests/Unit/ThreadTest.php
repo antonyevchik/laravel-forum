f8c84ccad4c3504afbc348c312f2cd73912befed
Anton Yevchik <av.yevchik@gmail.com>
2021-03-10 15:08:14 +0200

    Episode 70

--- tests/Unit/ThreadTest.php
+++ tests/Unit/ThreadTest.php
@@ -142,26 +142,5 @@ class ThreadTest extends TestCase
             $this->assertFalse($thread->hasUpdatesFor($user));
         });
     }
-
-    /**
-     * @test
-     */
-    function a_thread_records_ac_visit()
-    {
-        $thread = make('App\Thread', ['id' => 1]);
-
-        $thread->visits()->reset();
-
-        $this->assertSame(0, $thread->visits()->count());
-
-        $thread->visits()->record();
-
-        $this->assertEquals(1, $thread->visits()->count());
-
-        $thread->visits()->record();
-
-        $this->assertEquals(2, $thread->visits()->count());
-    }
-
 }
 
