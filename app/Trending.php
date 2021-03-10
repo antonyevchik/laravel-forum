3eed47694870ea7a9373e45368f86563dc0c965c
Anton Yevchik <av.yevchik@gmail.com>
2021-03-10 11:32:56 +0200

    Episode 67

---
+++ app/Trending.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App;
+
+use Illuminate\Support\Facades\Redis;
+
+class Trending
+{
+    /**
+     * @return array
+     */
+    public function get()
+    {
+        return array_map('json_decode', Redis::zrevrange($this->cacheKey(), 0, 4));
+    }
+
+    /**
+     * @param $thread
+     */
+    public function push($thread)
+    {
+        Redis::zincrby($this->cacheKey(), 1, json_encode([
+            'title' => $thread->title,
+            'path' => $thread->path(),
+        ]));
+    }
+
+    /**
+     * @return string
+     */
+    public function cacheKey()
+    {
+        return app()->environment('testing') ? 'testing_trending_threads' : 'trending_threads';
+    }
+
+    /**
+     * @return mixed
+     */
+    public function reset()
+    {
+        return Redis::del($this->cacheKey());
+    }
+}
