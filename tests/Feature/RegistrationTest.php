e4dc6d0f85a67c9018f92a2514a8574849ba129e
Anton Yevchik <av.yevchik@gmail.com>
2021-03-11 12:21:44 +0200

    Episode 72

--- tests/Feature/RegistrationTest.php
+++ tests/Feature/RegistrationTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Feature;
 
 use App\Mail\PleaseConfirmYourEmail;
+use App\User;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 
@@ -20,6 +21,28 @@ class RegistrationTest extends TestCase
 
         event(new Registered(create('App\User')));
 
-        Mail::assertSent(PleaseConfirmYourEmail::class, function ());
+        Mail::assertSent(PleaseConfirmYourEmail::class);
+    }
+
+    /** @test */
+    function users_can_fully_confirm_their_email_addresses()
+    {
+        $this->post('/register', [
+            'name' => 'John',
+            'email' => 'john@example.com',
+            'password' => 'foobarfoobar',
+            'password_confirmation' => 'foobarfoobar'
+        ]);
+
+        $user = User::whereName('John')->first();
+
+        $this->assertFalse($user->confirmed);
+        $this->assertNotNull($user->confirmation_token);
+
+        $response = $this->get('/register/confirm?token='.$user->confirmation_token);
+
+        $this->assertTrue($user->fresh()->confirmed);
+
+        $response->assertRedirect('/threads');
     }
 }
