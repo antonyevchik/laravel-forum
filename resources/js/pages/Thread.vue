9cac0a1e084de15ecbc9ba1db4a684a18afe7aac
Anton Yevchik <av.yevchik@gmail.com>
2021-11-18 11:18:29 +0200

    Episode 93

--- resources/js/pages/Thread.vue
+++ resources/js/pages/Thread.vue
@@ -11,6 +11,10 @@
                 repliesCount: this.thread.replies_count,
                 locked: this.thread.locked,
                 editing: false,
+                form: {
+                    title: this.thread.title,
+                    body: this.thread.body,
+                }
             }
         },
 
@@ -18,6 +22,25 @@
             toggleLock () {
                 axios[this.locked ? 'delete' : 'post']('/locked-threads/'+ this.thread.slug);
                 this.locked = ! this.locked;
+            },
+
+            cancel () {
+                this.form = {
+                    title: this.thread.title,
+                    body: this.thread.body,
+                };
+
+                this.editing = false;
+            },
+
+            update () {
+                axios.patch('/threads/' + this.thread.channel.slug + '/' + this.thread.slug, {
+                    title: this.form.title,
+                    body: this.form.body,
+                }).then(() => {
+                    this.editing = false;
+                    flash('Your thread has been updated.');
+                });
             }
         },
     }
