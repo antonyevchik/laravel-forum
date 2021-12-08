b54bbb0e547390d0d70c3bbc1bf0389b603114df
Anton Yevchik <av.yevchik@gmail.com>
2021-12-08 11:21:04 +0200

    Episode 93

--- resources/js/pages/Thread.vue
+++ resources/js/pages/Thread.vue
@@ -10,38 +10,50 @@
             return {
                 repliesCount: this.thread.replies_count,
                 locked: this.thread.locked,
-                editing: false,
-                form: {
-                    title: this.thread.title,
-                    body: this.thread.body,
-                }
+                title: this.thread.title,
+                body: this.thread.body,
+                form: {},
+                editing: false
             }
+        },
+
+        created () {
+            this.resetForm();
         },
 
         methods: {
             toggleLock () {
-                axios[this.locked ? 'delete' : 'post']('/locked-threads/'+ this.thread.slug);
+                let uri = `/locked-threads/${this.thread.slug}`;
+
+                axios[this.locked ? 'delete' : 'post'](uri);
+
                 this.locked = ! this.locked;
             },
 
             cancel () {
-                this.form = {
+                this.resetForm();
+            },
+
+            update () {
+                let uri = `/threads/${this.thread.channel.slug}/${this.thread.slug}`;
+
+                axios.patch(uri, this.form).then(() => {
+                    this.editing = false;
+                    this.title = this.form.title;
+                    this.body = this.form.body;
+
+                    flash('Your thread has been updated.');
+                });
+            },
+
+            resetForm () {
+                this.form =  {
                     title: this.thread.title,
                     body: this.thread.body,
                 };
 
                 this.editing = false;
             },
-
-            update () {
-                axios.patch('/threads/' + this.thread.channel.slug + '/' + this.thread.slug, {
-                    title: this.form.title,
-                    body: this.form.body,
-                }).then(() => {
-                    this.editing = false;
-                    flash('Your thread has been updated.');
-                });
-            }
         },
     }
 </script>
