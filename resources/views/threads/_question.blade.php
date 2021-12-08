7c279d72aafd0339524343812854afe98dedf9be
Anton Yevchik <av.yevchik@gmail.com>
2021-12-08 11:21:04 +0200

    Episode 93

--- resources/views/threads/_question.blade.php
+++ resources/views/threads/_question.blade.php
@@ -13,7 +13,7 @@
         <div class="level">
             <button class="btn btn-sm btn-outline-primary level-item" @click="editing = true" v-show="! editing">Edit</button>
             <button class="btn btn-sm btn-primary level-item" @click="update">Update</button>
-            <button class="btn btn-sm btn-outline-primary level-item" @click="cancel">Cancel</button>
+            <button class="btn btn-sm btn-outline-primary level-item" @click="resetForm">Cancel</button>
             @can('update',$thread)
                 <form action="{{$thread->path()}}" method="POST" class="ml-auto">
                     {{csrf_field()}}
@@ -32,12 +32,12 @@
         <div class="level">
             <img src="{{asset($thread->creator->avatar_path)}}" alt="{{ $thread->creator->name }}" width="3%" height="3%" class="mr-1">
             <span class="flex">
-                <a href="{{route('profile', $thread->creator)}}"> {{$thread->creator->name}}</a> posted: <span v-text="form.title"></span>
+                <a href="{{route('profile', $thread->creator)}}"> {{$thread->creator->name}}</a> posted: <span v-text="title"></span>
             </span>
         </div>
     </div>
-    <div class="card-body" v-text="form.body"></div>
-    <div class="card-footer">
+    <div class="card-body" v-text="body"></div>
+    <div class="card-footer" v-if="authorize('owns', thread)">
         <button class="btn btn-sm btn-outline-primary" @click="editing = true">Edit</button>
     </div>
 </div>
